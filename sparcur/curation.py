1ba8ad75bc50afda88b725bfe4361b9768c6cbce
Tom Gillespie <tgbugs@gmail.com>
2019-03-19 11:01:36 -0700

    curation remove SparcMI import for now

--- sparcur/curation.py
+++ sparcur/curation.py
@@ -20,7 +20,7 @@ from pyontutils.namespaces import OntCuries, makeNamespaces, TEMP
 from pyontutils.closed_namespaces import rdf, rdfs, owl, skos, dc
 from protcur.analysis import parameter_expression
 from protcur.core import annoSync
-from protcur.analysis import protc, Hybrid, SparcMI
+from protcur.analysis import protc, Hybrid
 from pysercomb.pyr.units import ProtcParameter
 from scibot.utils import resolution_chain
 from hyputils.hypothesis import group_to_memfile, HypothesisHelper
@@ -2193,7 +2193,7 @@ class CurationReport:
 def populate_annos(group_name='sparc-curation'):
     group = devconfig.secrets('hypothesis', 'group', group_name)
     get_annos, annos, stream_thread, exit_loop = annoSync(group_to_memfile(group),
-                                                          helpers=(HypothesisHelper, Hybrid, protc, SparcMI),
+                                                          helpers=(HypothesisHelper, Hybrid, protc),
                                                           group=group)
 
     [protc(a, annos) for a in annos]
