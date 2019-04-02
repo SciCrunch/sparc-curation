90ba4542769ea99ec23b4477d4d56e7214f7f16e
Tom Gillespie <tgbugs@gmail.com>
2019-04-02 01:11:42 -0400

    paths.py added docstring

--- sparcur/paths.py
+++ sparcur/paths.py
@@ -1,3 +1,26 @@
+"""sparcur extensions to pathlib
+
+sparcur makes extensive use of the pathlib Path object (and friends)
+by augmenting the base PosixPath object with additional functionality
+such as getting and setting xattrs, syncing with other mapped paths etc.
+
+In essence there are 3 ways that a Path object can be used: Local, Cache, and Remote.
+Local paths return data and metadata that are local the the current computer.
+Cache paths return local metadata about remote objects (such as their remote id).
+Remote objects provide an interface to remote data that is associated with a path.
+
+Remote paths should be back by another object which is the representation of the
+remote according to the remote's APIs.
+
+Remote paths are only intended to provide a 1:1 mapping, so list(local.data) == list(remote.data)
+should always be true if everything is in sync.
+
+If there is additional metadata that is associated with a local path then that is
+represented in the layer above this one (currently FThing, in the future a validation Stage).
+That said, it does seem like we need a more formal place that can map between all these
+things rather than always trying to derive the mappings from data embedded (bound) to
+the derefereced path object. """
+
 import sys
 import pickle
 import struct
