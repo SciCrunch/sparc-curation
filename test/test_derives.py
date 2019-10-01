d73651755279771c9d15629ecea380ff4bd0e3a8
Tom Gillespie <tgbugs@gmail.com>
2019-10-01 11:41:55 -0700

    remove test_dsci as completeness index has been reworked and removed

--- test/test_derives.py
+++ test/test_derives.py
@@ -3,12 +3,4 @@ from sparcur import schemas as sc
 from sparcur.derives import Derives as De
 
 class TestDerives(unittest.TestCase):
-    def test_dsci(self):
-        schema = sc.DatasetSchema.schema
-        subschemas = {'dataset_description':sc.DatasetDescriptionSchema.schema,
-                        'submission':sc.SubmissionSchema.schema,
-                        'subjects':sc.SubjectsSchema.schema,}
-        inputs = {'inputs':
-                  {'errors': [1, 2, 3],}}
-
-        De.submission_completeness_index(schema, subschemas, inputs)
+    pass
