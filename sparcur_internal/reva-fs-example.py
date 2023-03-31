32cce736aee02b8276383002ba63f9317eccc54f
Tom Gillespie <tgbugs@gmail.com>
2023-03-31 16:01:09 -0700

    reva-fs-example updated conventions, generate shell scrip
    setting shell=True in main args will output a bash scrip to generate
    the hierarchy, still needs to be wired up to pass paths on cli etc.
    
    while sds does not support alternate prefixes for types, we can think
    about the hierarchy presented here as sub- sam- seg- site- sec- where
    seg- and sec- are subtypes of sam-
    
    some design decisions
    1. use sites for ultrasound on intanct cadaver as site-l and site-r
       to clarify that ultrasound was not collected on sam-{l,r}
    2. use a single histology
       this allows us to use the same structure for the 4 vagus nerves
       where sections are taken every 250um and the rest that are taken
       only near one of the marked sites, in this approach the mapping of
       files to section identifiers will be done in the manifest to avoid
       increasing the number of folders
    3. use l and r by themselves to identify the vagus samples, the v was
       redundant and not helpful
    4. prefix subject with f to identify the group they came from so that
       all subjects in reva could be in the same file system level
    
    suggestion for how to organize datasets so that the metadata can
    coexist and minimal/no restructuring will be required, this adds
    overhead of roughly 300 folders total for primary, derivative, and
    source for 100 subjects compared to a version where pri, der, so, and
    metadata are deduplicate, the deduped versions adds significant
    operational complexity because it means that the structure has to be
    transformed into the SDS structure, so go with the one below
    
    /dataset-sub-f001/primary/sub-f001
    /dataset-sub-f001/derivative/sub-f001
    /dataset-sub-f001/samples.json
    /dataset-sub-f001/subjects.json
    /dataset-sub-f001/sites.xlsx
    /dataset-sub-f001/dataset_description.json
    /dataset-sub-f001/submission.json
    
    /dataset-sub-f002/primary/sub-f002
    /dataset-sub-f002/derivative/sub-f002
    /dataset-sub-f002/samples.json
    /dataset-sub-f002/subjects.json
    /dataset-sub-f002/sites.xlsx
    /dataset-sub-f002/dataset_description.json
    /dataset-sub-f002/submission.json
    ...
    /dataset-sub-f100/

--- sparcur_internal/reva-fs-example.py
+++ sparcur_internal/reva-fs-example.py
@@ -1,26 +1,40 @@
-# number of segments per laterality
-# this is expected to vary per subject
-lat_seg = {
-    'l': 4,
-    'r': 3,
-}
-# mapping between sites and segments for each laterality
-# this is expected to vary per subject
-lat_seg_site = {
-    'l': {
-        1: ['nd'],
-        2: ['cg', 'eg'],
-        3: [],
-        4: [],
+# example data for subject 1
+subject_spec_1 = dict(
+    id = 'sub-f001',
+    # number of segments per laterality
+    # this is expected to vary per subject
+    lat_seg = {
+        'l': 4,
+        'r': 3,
     },
-    'r': {
-        1: ['nd'],
-        2: ['cg', 'eg'],
-        3: [],
+    # mapping between sites and segments for each laterality
+    # this is expected to vary per subject
+    lat_seg_site = {
+        'l': {
+            1: ['nd'],
+            2: ['cg', 'eg'],
+            3: [],
+            4: [],
+        },
+        'r': {
+            1: ['nd'],
+            2: ['cg', 'eg'],
+            3: [],
+        },
     },
+)
+
+site_conventions = {
+    'nd': 'nodose ganglion',
+    'eg': 'esophogeal ganglaion',
+    'cg': 'coeliac ganglion',
 }
-# mapping of slide number to protocol type
-slide_type = {
+
+# mapping of section number to protocol type
+# FIXME if can this vary per subject then we need to embed it in the subject_spec
+# if it can vary per site then we likely need theme an variations with per subject
+# and then allow override at site
+section_type = {
     1: 'he',
     2: 'ihc-chat',
     3: 'ihc-th',
@@ -31,87 +45,153 @@ slide_type = {
     8: 'ihc-nav1',
 }
 
-# the expected data files per type
+# the expected data files per section type
 type_example_files = {
     'he': ['20x.tiff', '100x.tiff'],
     'em': ['big-em-image.tiff',],
     'ihc': ['20x.tiff', '100x.tiff'],
 }
 
+
 # conventions for padding identifiers can be discussed, right now they are unpadded
-def make_tree(path, pdo, subject, lat_seg, lat_seg_site):
+def make_tree(path, pdo, subject_spec, ultrasound_as_site=True, histology_as_folder=True,
+              mct_primary_name='stacked',  # change as needed/inspired
+              n_virt_sec=7,
+              n_ihc_fasc=10,  # less than 100 per section
+              ):
     primary = pdo == 'primary'
     derived = pdo == 'derived'
-    other = not primary and not derived
+    source = pdo == 'source'
+    other = not (primary or derived or source)
+
+    subject = subject_spec['id']
+    lat_seg = subject_spec['lat_seg']
+    lat_seg_site = subject_spec['lat_seg_site']
+    # section_type = subject_spec['section_type']  # XXX see note above
+    # site_section_type = subject_spec['site_section_type']  # XXX note above
+    n_sec = len(section_type)
+
     dirs = []
     files = []
+    us_type = 'site' if ultrasound_as_site else 'sam'
+    # laterality
     for lat, nseg in lat_seg.items():
-        p = f'sam-v{lat}/ultrasound'  # TODO we may need site-vl and site-vr for this
+        # ultrasound
+        p = f'{us_type}-{lat}/ultrasound'  # TODO we may need site-vl and site-vr for this
         dirs.append(p)
         if primary:
             f = p + '/image.image'
             files.append(f)
         elif derived:
             pass  # content unknown at this time
+        elif source:
+            pass
         else:
             pass
+        # segments
         for seg in range(1, nseg + 1):
-            p = f'sam-v{lat}/sam-v{lat}-seg-{seg}/microct'
+            # microct
+            p = f'sam-{lat}/sam-{lat}-seg-{seg}/microct'
             dirs.append(p)
             if primary:
-                f = p + '/image.image'
-                files.append(f)
+                for ext in ('jpx', 'tiff'):
+                    f = p + f'/{mct_primary_name}.{ext}'
+                    files.append(f)
             elif derived:
-                pass  # content unknown at this time
+                # there will be one output file per 2d section from the microct
+                # the fascicles would be identified per "virtual section"
+                for n in range(1, n_virt_sec + 1):
+                    files.append(p + f'/vsec-rois-{n}.tabular')
+            elif source:
+                # right now only seeing 2d coronoal slices come out of the CT software
+                # in principle other sections could come out
+                # assume that the raw tilt series exists and will be provided
+                files.append(p + f'/tilt-series.image')
+                for n in range(1, n_virt_sec + 1):
+                    f = p + f'/raw-{n}.image'
+                    files.append(f)
             else:
                 pass
+            # sites
             for site in lat_seg_site[lat][seg]:
-                for slide in range(1, 9):
-                    p = f'sam-v{lat}/sam-v{lat}-seg-{seg}/sam-v{lat}-{site}-s-{slide}'
+                if histology_as_folder:
+                    # if we go with histology as folder we will want to use
+                    # the manifest to map to sections and probably put the
+                    # section id in the file name as a cross reference
+                    p = f'sam-{lat}/sam-{lat}-seg-{seg}/histology'
                     dirs.append(p)
-                    type = slide_type[slide].split('-')[0]
+                    # TODO generate the manifest mapping sec seg sam
+                    files.append(p + '/manifest.tabular')
+                # section_type = site_section_type[site]
+                # sections
+                for sec, _type in section_type.items():
+                    if not histology_as_folder:
+                        p = f'sam-{lat}/sam-{lat}-seg-{seg}/sam-{lat}-{site}-sec-{sec}'
+                        dirs.append(p)
+
+                    type = _type.split('-')[0]
                     if primary:
-                        for f in type_example_files[type]:
-                            files.append(p + f'/{f}')
+                        if histology_as_folder:
+                            # primary section files
+                            for _f in type_example_files[type]:
+                                f = f'{lat}-{site}-sec-{sec}-{_f}'
+                                files.append(p + f'/{f}')
+                        else:
+                            for f in type_example_files[type]:
+                                files.append(p + f'/{f}')
                     elif derived:
-                        files.append(p + f'/manifest.xlsx')  # put here to reduce number of files
+                        files.append(p + f'/fascicles.csv')
                         # TODO manifest at higher level to further reduce file overhead ??
+                        files.append(p + f'/manifest.tabular')  # put here to reduce number of files
                         _bp = p
-                        for n in range(1,10):  # 10 is almost certainly too small
+                        for n in range(1, n_ihc_fasc + 1):
                             # TODO what about nested fascicles? do you see them?
-                            p = _bp + f'/roi-fasc-{n:0>4}'
+                            # TODO naming for this is incomplete
+                            p = _bp + f'/roi-{site}-sec-{sec}-fasc-{n:0>3}'
                             dirs.append(p)
                             files.append(p + f'/crop.image')
                             files.append(p + f'/human.image')
                             files.append(p + f'/machine.image')
                             files.append(p + f'/fibers.tabular')
+                        p = _bp  # histology foloder issue
+                    elif source:
+                        pass
                     else:
                         pass
 
-    tld = 'primary' if primary else ('derivative' if derived else 'wat')
+    tld = 'primary' if primary else ('derivative' if derived else ('source' if source else 'wat'))
     path_dirs = [path / tld / subject / d for d in dirs]
     path_files = [path / tld / subject / f for f in files]
     return path_dirs, path_files
 
 
-def main(subject='sub-f001'):
+def main(subject_id='sub-f001', make=True, shell=False,
+         ultrasound_as_site=True, histology_as_folder=True):
     from pathlib import Path
-
+    spec_index = {'sub-f001': subject_spec_1}
+    subject_spec = spec_index[subject_id]
     #path = Path('.').expanduser()
-    path = Path('~/git/sparc-curation/resources/examples/reva-4/').expanduser()
+    path = Path('~/git/sparc-curation/resources/examples/reva-11/').expanduser()
     dirs, files = [], []
-    for tld in ('primary', 'derived'):
-        ds, fs = make_tree(path, tld, subject, lat_seg, lat_seg_site)
+    for tld in ('primary', 'derived', 'source'):
+        ds, fs = make_tree(path, tld, subject_spec, ultrasound_as_site, histology_as_folder)
         dirs.extend(ds)
         files.extend(fs)
 
-    make = False
     if make:
         [d.mkdir(parents=True, exist_ok=True)for d in dirs]
         [f.touch() for f in files]
+    elif shell:
+        script = f'#!/usr/bin/env bash\npushd {path.as_posix()!r} || exit $?\n'
+        script += '\n'.join([f'mkdir -p {d.relative_to(path).as_posix()!r}' for d in dirs])
+        script += '\npopd'
+        print(script)
+    else:
+        df = [[p.as_posix()] for p in [*dirs, *files]]
+        from pprint import pprint
+        pprint(df)
+        return df
 
-    return [[p.as_posix()] for p in [*dirs, *files]]
 
 if __name__ == '__main__':
     out = main()
-    #return out
